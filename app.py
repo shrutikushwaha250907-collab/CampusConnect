b90e3933f6290543b5690fc314b3e5a86c0b6ba8
shrutikushwaha250907-collab <shruti.kushwaha25.09.07@gmail.com>
2026-07-25 22:54:32 +0530

    Added login system and updated UI

--- app.py
+++ app.py
@@ -1,23 +1,105 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, flash
+from models import db, User
+from config import Config
 
 app = Flask(__name__)
 
-app.secret_key = "campusconnect"
+app.config.from_object(Config)
 
-# Home Page
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+# -------------------- Home --------------------
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
-# Login Page
-@app.route("/login")
+
+# -------------------- Register --------------------
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        fullname = request.form["fullname"]
+        email = request.form["email"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash("Username already exists!")
+            return redirect("/register")
+
+        new_user = User(
+            fullname=fullname,
+            email=email,
+            username=username,
+            password=password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration Successful!")
+        return redirect("/login")
+
+    return render_template("register.html")
+
+# -------------------- Login --------------------
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            username=username,
+            password=password
+        ).first()
+
+        if user:
+
+            session["user"] = user.username
+
+            return redirect("/dashboard")
+
+        flash("Invalid Username or Password")
+
     return render_template("login.html")
 
-# Register Page
-@app.route("/register")
-def register():
-    return render_template("register.html")
+
+# -------------------- Dashboard --------------------
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    return render_template(
+        "dashboard.html",
+        username=session["user"]
+    )
+
+
+# -------------------- Logout --------------------
+
+@app.route("/logout")
+def logout():
+
+    session.pop("user", None)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
